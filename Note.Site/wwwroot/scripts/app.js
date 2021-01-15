764e1ccf84a3d2c1b4e30d6d73349ec0b685fba5
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2021-01-15 00:15:48 +0000

    task: added minification for both css and js

--- Note.Site/wwwroot/scripts/app.js
+++
@@ -1,8 +0,0 @@
-﻿var renderMarkdown = function () {
-    let textareaElement = document.getElementById('div_editor');
-    let previewElement = document.getElementById('div_markdown');
-
-    textareaElement.addEventListener("keyup", (ev) => {
-        previewElement.innerHTML = marked(ev.target.value);
-    });
-}
